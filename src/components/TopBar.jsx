c08c8b37c1a24119804c20f3c2a11f3d6920ab2f
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2024-04-23 18:02:44 +0500

    change font size

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -6,7 +6,7 @@ export default function TopBar() {
     <section className="fixed bg-base-100 max-w-2xl z-50 top-0 flex w-full justify-between items-center px-5 py-3 ">
       <div className="flex justify-between items-center w-full ">
         <img src={user_group} alt="contacts" className="w-6 h-6" />
-        <h1 className="font-semibold text-xl font-[khand]">RECENT MESSAGES</h1>
+        <h1 className="font-semibold font-[khand]">RECENT MESSAGES</h1>
         <img src={archive} alt="archive" className="w-6 h-6" />
       </div>
     </section>
